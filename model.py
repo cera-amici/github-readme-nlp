49b19b7488af2e15591507095d3e263d42810050
Sam Callahan <callahan.l.sam@gmail.com>
2019-12-12 09:29:18 -0600

    Completed model and pickled it

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support as score
+import pickle
 
 from prepare import prep
 
@@ -29,64 +30,69 @@ def get_words(df, language=None):
     words['idf'] = words.index.to_series().apply(idf)
     return words
 
-words = get_words(df)
+def store_model(model_object):
+    with open('model.pickle', 'wb') as fp:
+        pickle.dump(model_object, fp)
 
-tfidf = TfidfVectorizer()
-x = tfidf.fit_transform(df.lemmatized)
-y = df.language
+def get_model():
+    with open ('model.pickle', 'rb') as fp:
+        model = pickle.load(fp)
+    return model
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=.2)
+if __name__ == '__main__':
+    words = get_words(df)
 
-train = pd.DataFrame(dict(actual=y_train))
-test = pd.DataFrame(dict(actual=y_test))
+    tfidf = TfidfVectorizer()
+    x = tfidf.fit_transform(df.lemmatized)
+    y = df.language
 
-train['predicted'] = dt.predict(x_train)
-test['predicted'] = dt.predict(x_test)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=.2)
 
-print(classification_report(train.actual, train.predicted))
+    train = pd.DataFrame(dict(actual=y_train))
+    test = pd.DataFrame(dict(actual=y_test))
 
-print(classification_report(test.actual, test.predicted))
+    score_diff = 1
+    leaf = 0
 
-score_diff = 1
-leaf = 1
+    while score_diff > .06:
+        leaf += 1
+        dt = DecisionTreeClassifier(criterion='entropy', min_samples_leaf=leaf)
+        dt.fit(x_train, y_train)
+        train['predicted'] = dt.predict(x_train)
+        test['predicted'] = dt.predict(x_test)
+        train_acc = score(train.actual, train.predicted)[2].mean()
+        test_acc = score(test.actual, test.predicted)[2].mean()
+        score_diff = train_acc - test_acc
 
-while score_diff > .06:
-    dt = DecisionTreeClassifier(criterion='entropy', min_samples_leaf=leaf)
-    dt.fit(x_train, y_train)
-    train['predicted'] = dt.predict(x_train)
-    test['predicted'] = dt.predict(x_test)
-    train_acc = score(train.actual, train.predicted)[2].mean()
-    test_acc = score(test.actual, test.predicted)[2].mean()
-    score_diff = train_acc - test_acc
-    leaf += 1
+    print(f'leaf = {leaf}')
+    print(f'train acc = {train_acc}')
+    print(f'test acc = {test_acc}')
 
-print(f'leaf = {leaf}')
-print(f'train acc = {train_acc}')
-print(f'test acc = {test_acc}')
+    store_model(dt)
 
-score_diff = 1
-leaf = 1
+# score_diff = 1
+# leaf = 1
 
-while score_diff > .06:
-    rf = RandomForestClassifier(criterion='entropy', min_samples_leaf=leaf, random_state=123)
-    rf.fit(x_train, y_train)
-    train['predicted'] = rf.predict(x_train)
-    test['predicted'] = rf.predict(x_test)
-    train_acc = score(train.actual, train.predicted)[2].mean()
-    test_acc = score(test.actual, test.predicted)[2].mean()
-    score_diff = train_acc - test_acc
-    leaf += 1
+# while score_diff > .06:
+#     rf = RandomForestClassifier(criterion='entropy', min_samples_leaf=leaf, random_state=123)
+#     rf.fit(x_train, y_train)
+#     train['predicted'] = rf.predict(x_train)
+#     test['predicted'] = rf.predict(x_test)
+#     train_acc = score(train.actual, train.predicted)[2].mean()
+#     test_acc = score(test.actual, test.predicted)[2].mean()
+#     score_diff = train_acc - test_acc
+#     leaf += 1
 
-rf = RandomForestClassifier(criterion='entropy', min_samples_leaf=19, random_state=123)
-rf.fit(x_train, y_train)
-train['predicted'] = rf.predict(x_train)
-test['predicted'] = rf.predict(x_test)
-train_acc = score(train.actual, train.predicted)[2].mean()
-test_acc = score(test.actual, test.predicted)[2].mean()
-score_diff = train_acc - test_acc
+# rf = RandomForestClassifier(criterion='entropy', min_samples_leaf=19, random_state=123)
+# rf.fit(x_train, y_train)
+# train['predicted'] = rf.predict(x_train)
+# test['predicted'] = rf.predict(x_test)
+# train_acc = score(train.actual, train.predicted)[2].mean()
+# test_acc = score(test.actual, test.predicted)[2].mean()
+# score_diff = train_acc - test_acc
 
-print(f'leaf = {leaf}')
-print(f'train acc = {train_acc}')
-print(f'test acc = {test_acc}')
+# print(f'leaf = {leaf}')
+# print(f'train acc = {train_acc}')
+# print(f'test acc = {test_acc}')
 
-# scaled_df = 
+# scaled_df = m
