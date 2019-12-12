b828d7b802bb696938393b966ba7b9e3b85a314d
Sam Callahan <callahan.l.sam@gmail.com>
2019-12-12 10:59:39 -0600

    Worked on notebook

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support as score
 import pickle
+import os
 
 from prepare import prep
 
@@ -18,16 +19,22 @@ def idf(word):
     idf = np.log(n_docs / n_occurrences)
     return idf
 
-def get_words(df, language=None):
-    if language is not None:
-        df = df[df.language == language]
-    
-    raw_count = pd.Series(' '.join(df.lemmatized).split()).value_counts()
+def get_words(df, fresh=False):
+    if fresh or not os.path.exists('words.pickle'):
+        raw_count = pd.Series(' '.join(df.lemmatized).split()).value_counts()
 
-    words = pd.DataFrame({'raw_count': raw_count})
-    words['frequency'] = words.raw_count / words.raw_count.sum()
-    words['augmented_frequency'] = words.frequency / words.frequency.max()
-    words['idf'] = words.index.to_series().apply(idf)
+        words = pd.DataFrame({'raw_count': raw_count})
+        words['frequency'] = words.raw_count / words.raw_count.sum()
+        words['augmented_frequency'] = words.frequency / words.frequency.max()
+        words['idf'] = words.index.to_series().apply(idf)
+
+        with open('words.pickle', 'wb') as fp:
+            pickle.dump(words, fp)  
+
+    else:
+        with open('words.pickle', 'rb') as fp:
+            words = pickle.load(fp)
+
     return words
 
 def store_model(model_object):
@@ -39,54 +46,32 @@ def get_model():
         model = pickle.load(fp)
     return model
 
-if __name__ == '__main__':
-    words = get_words(df)
-
+def prevent_overfitting(tree_function, df, store=False):
     tfidf = TfidfVectorizer()
     x = tfidf.fit_transform(df.lemmatized)
     y = df.language
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=.2, random_state=42)
 
     train = pd.DataFrame(dict(actual=y_train))
     test = pd.DataFrame(dict(actual=y_test))
 
     score_diff = 1
     leaf = 0
-
+    
     while score_diff > .06:
         leaf += 1
-        dt = DecisionTreeClassifier(criterion='entropy', min_samples_leaf=leaf)
-        dt.fit(x_train, y_train)
-        train['predicted'] = dt.predict(x_train)
-        test['predicted'] = dt.predict(x_test)
-        train_acc = score(train.actual, train.predicted)[2].mean()
-        test_acc = score(test.actual, test.predicted)[2].mean()
-        score_diff = train_acc - test_acc
-
-    print(f'leaf = {leaf}')
-    print(f'train acc = {train_acc}')
-    print(f'test acc = {test_acc}')
-
-    store_model(dt)
-
-def prevent_overfitting(tree_function, train, test):
-    score_diff = 1
-    leaf = 1
-
-    while score_diff > .06:
-        model = tree_function(criterion='entropy', min_samples_leaf=leaf, random_state=123)
+        model = tree_function(criterion='entropy', min_samples_leaf=leaf, random_state=42)
         model.fit(x_train, y_train)
         train['predicted'] = model.predict(x_train)
         test['predicted'] = model.predict(x_test)
         train_acc = score(train.actual, train.predicted)[2].mean()
         test_acc = score(test.actual, test.predicted)[2].mean()
         score_diff = train_acc - test_acc
-        leaf += 1
 
     print(f'leaf = {leaf}')
     print(f'train acc = {train_acc}')
     print(f'test acc = {test_acc}')
+    if store:
+        store_model(model)
     return model
-
-# scaled_df = m
